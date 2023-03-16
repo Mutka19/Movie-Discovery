2977a331b5739f835d30923a4cb2c2d6d2e45bfa
dmutka <daemon.mutka98@gmail.com>
2023-03-16 15:48:29 -0500

    Reformatted to BLACK standards

--- flask_stuff.py
+++ flask_stuff.py
@@ -7,15 +7,17 @@ app = flask.Flask("Movie Discovery")
 @app.route("/")
 def index():
     movies = get_3_default_movies()
-    movie_title = movies[0]['title']
-    movie_overview = movies[0]['overview']
-    movie_genres = ", ".join([movie['name'] for movie in movies[0]['genres']])
-    movie_img = get_movie_img(movies[0]['poster_path'])
+    movie_title = movies[0]["title"]
+    movie_overview = movies[0]["overview"]
+    movie_genres = ", ".join([movie["name"] for movie in movies[0]["genres"]])
+    movie_img = get_movie_img(movies[0]["poster_path"])
     return flask.render_template(
-        "index.html", 
-        Title=movie_title, 
-        Overview=movie_overview, 
-        Genres=movie_genres, 
-        IMG=movie_img)
+        "index.html",
+        Title=movie_title,
+        Overview=movie_overview,
+        Genres=movie_genres,
+        IMG=movie_img,
+    )
+
 
 app.run(debug=True)
--- movie_discovery.py
+++ movie_discovery.py
@@ -14,16 +14,15 @@ def get_3_default_movies():
 
     for ID in MOVIE_IDS:
         request = rq.get(
-        TMBD_BASE_URL + TMBD_MOVIE_PATH + ID,
-        params={
-            "api_key": os.getenv("TMBD_API_KEY")
-        }
+            TMBD_BASE_URL + TMBD_MOVIE_PATH + ID,
+            params={"api_key": os.getenv("TMBD_API_KEY")},
         )
         MOVIE_LIST.append(request.json())
     return MOVIE_LIST
 
+
 def get_movie_img(poster_path):
-    TMBD_BASE_IMAGE_URL ="https://image.tmdb.org/t/p/"
+    TMBD_BASE_IMAGE_URL = "https://image.tmdb.org/t/p/"
     TMBD_IMG_SIZE = "w500/"
 
     return TMBD_BASE_IMAGE_URL + TMBD_IMG_SIZE + poster_path
@@ -41,10 +40,10 @@ def search_for_movie(movie):
     return request.json()
 
 
-#print(search_for_movie("Shrek"))
-#names = ", ".join([sublist["name"] for sublist in get_3_default_movies()[0]["genres"]])
-#movies = get_3_default_movies()
-#print(movies[0])
-#print(get_movie_img(movies[0]['poster_path']))
-#print(names)
-#print(get_3_default_movie_images())
+# print(search_for_movie("Shrek"))
+# names = ", ".join([sublist["name"] for sublist in get_3_default_movies()[0]["genres"]])
+# movies = get_3_default_movies()
+# print(movies[0])
+# print(get_movie_img(movies[0]['poster_path']))
+# print(names)
+# print(get_3_default_movie_images())
