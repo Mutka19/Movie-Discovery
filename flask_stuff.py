819879d515317b1ea9a6ae1a3a1697be15bae366
dmutka <daemon.mutka98@gmail.com>
2023-03-18 10:23:16 -0500

    Changed render_template to take new WikiLink argument

--- flask_stuff.py
+++ flask_stuff.py
@@ -1,22 +1,26 @@
 import flask
-from movie_discovery import get_3_default_movies, get_movie_img
+import random
+from movie_discovery import get_3_default_movies, get_movie_img, get_wiki_link
 
 app = flask.Flask("Movie Discovery")
 
 
 @app.route("/")
 def index():
+    index = random.randrange(0,2)
     movies = get_3_default_movies()
-    movie_title = movies[0]["title"]
-    movie_overview = movies[0]["overview"]
-    movie_genres = ", ".join([movie["name"] for movie in movies[0]["genres"]])
-    movie_img = get_movie_img(movies[0]["poster_path"])
+    movie_title = movies[index]["title"]
+    movie_overview = movies[index]["overview"]
+    movie_genres = ", ".join([movie["name"] for movie in movies[index]["genres"]])
+    movie_img = get_movie_img(movies[index]["poster_path"])
+    link = get_wiki_link(movies[index]['title'])
     return flask.render_template(
         "index.html",
         Title=movie_title,
         Overview=movie_overview,
         Genres=movie_genres,
         IMG=movie_img,
+        WikiLink=link
     )
 
 
