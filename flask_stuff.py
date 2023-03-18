6255aa380a301f88818f63571b9ecc5f30e54a58
dmutka <daemon.mutka98@gmail.com>
2023-03-18 11:51:08 -0500

    Added user info handling funtionality, user can now input a movie name and have the info for that movie displayed on the site

--- flask_stuff.py
+++ flask_stuff.py
@@ -1,27 +1,57 @@
 import flask
 import random
-from movie_discovery import get_3_default_movies, get_movie_img, get_wiki_link
+from movie_discovery import (
+    get_3_default_movies,
+    get_movie_img,
+    get_wiki_link,
+    search_for_movie,
+)
 
 app = flask.Flask("Movie Discovery")
 
 
 @app.route("/")
 def index():
-    index = random.randrange(0,2)
+    index = random.randrange(0, 3)
     movies = get_3_default_movies()
     movie_title = movies[index]["title"]
     movie_overview = movies[index]["overview"]
     movie_genres = ", ".join([movie["name"] for movie in movies[index]["genres"]])
     movie_img = get_movie_img(movies[index]["poster_path"])
-    link = get_wiki_link(movies[index]['title'])
+    link = get_wiki_link(movies[index]["title"])
     return flask.render_template(
         "index.html",
         Title=movie_title,
         Overview=movie_overview,
         Genres=movie_genres,
         IMG=movie_img,
-        WikiLink=link
+        WikiLink=link,
     )
+
+
+@app.route("/", methods=["POST"])
+def handle_user_info():
+    form_data = flask.request.form
+    movie_title = form_data["Movie"]
+    if len(movie_title) == 0:
+        flask.flash("Please enter a movie title before submitting")
+        return flask.redirect(flask.url_for("index"))
+    else:
+        movie = search_for_movie(movie_title)
+        movie_title = movie["title"]
+        movie_overview = movie["overview"]
+        movie_genres = ", ".join([genres["name"] for genres in movie["genres"]])
+        movie_img = get_movie_img(movie["poster_path"])
+        link = get_wiki_link(movie["title"])
+        return flask.render_template(
+            "index.html",
+            Title=movie_title,
+            Overview=movie_overview,
+            Genres=movie_genres,
+            IMG=movie_img,
+            WikiLink=link,
+        )
+    return
 
 
 app.run(debug=True)
