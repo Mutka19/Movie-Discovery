938bed9b750c02e4c7b71dc06e216b2c580de49b
dmutka <daemon.mutka98@gmail.com>
2023-04-06 13:12:14 -0500

    Added sign out functionality

--- movie-discovery.py
+++ movie-discovery.py
@@ -2,6 +2,7 @@ import flask
 import random
 import os
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 from api_handler import (
     get_3_default_movies,
     get_movie_img,
@@ -21,14 +22,24 @@ db = SQLAlchemy(app)
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
         return "<User %r>" % self.username
 
-    def check_password(self, password):
+    def validate(self, password):
         return self.password == password
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    review_text = db.Column(db.String(280), nullable=False)
+    film_id = db.Column(db.Integer, nullable=False)
+    user_name = db.Column(db.String(50), nullable=False)
+    rating = db.Column(db.Integer)
+
+def check_login():
+    return "username" in flask.session
 
 
 @app.route("/")
@@ -42,6 +53,7 @@ def index():
     link = get_wiki_link(movie["title"])
     m_id=movie["id"]
     username = flask.session.get("username")
+    review = Review.query.filter_by(film_id=m_id).order_by(func.random()).first()
     return flask.render_template(
         "index.html",
         Title=movie_title,
@@ -50,7 +62,8 @@ def index():
         IMG=movie_img,
         WikiLink=link,
         username=username,
-        movie_id=m_id
+        movie_id=m_id,
+        review=review
     )
 
 
@@ -68,8 +81,9 @@ def handle_user_info():
         movie_genres = ", ".join([genres["name"] for genres in movie["genres"]])
         movie_img = get_movie_img(movie["poster_path"])
         link = get_wiki_link(movie["title"])
-        id = movie["id"]
+        m_id = movie["id"]
         username = flask.session.get("username")
+        review = Review.query.filter_by(film_id=m_id).order_by(func.random()).first()
         return flask.render_template(
             "index.html",
             Title=movie_title,
@@ -78,27 +92,24 @@ def handle_user_info():
             IMG=movie_img,
             WikiLink=link,
             username=username,
-            movie_id=id,
+            movie_id=m_id,
+            review=review
         )
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if flask.request.method == "POST":
-        # Handle form submission
         username = flask.request.form["username"]
         password = flask.request.form["password"]
         print(username, password)
 
-        # Authenticate user
         user = Person.query.filter_by(username=username).first()
-        if user is not None and user.check_password(password):
-            # Login successful, redirect to dashboard
+        if user is not None and user.validate(password):
             flask.session["user_id"] = user.id
             flask.session["username"] = username
             return flask.redirect(flask.url_for("index"))
         else:
-            # Login failed, show error message
             error_message = "Invalid username or password"
             return flask.render_template("login.html", error_message=error_message)
 
@@ -109,37 +120,46 @@ def login():
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if flask.request.method == "POST":
-        # Handle form submission
         username = flask.request.form["username"]
         password = flask.request.form["password"]
 
-        # Add the new person to the database
+        if Person.query.filter_by(username=username).first():
+            error_message = "This username already exists, please choose a different username!"
+            return flask.render_template("signup.html", error_message=error_message)
+
         new_user = Person(username=username, password=password)
 
         db.session.add(new_user)
         db.session.commit()
-        # Redirect to the login page or some other page
         return flask.redirect("/login")
     else:
-        # Render the sign up page
         return flask.render_template("signup.html")
 
 
 @app.route("/review/<int:movie_id>", methods=["GET", "POST"])
 def review(movie_id):
+    if not check_login():
+        return flask.render_template("login.html", error_message="Must be logged in to write a review!")
+    
     if flask.request.method == "POST":
-        # Get the review text from the form submission
-        review_text = flask.request.form["review-text"]
 
-        # Store the review in the database for the specified movie ID
-        # (You would need to write code to do this based on your database schema)
+        review_text = flask.request.form["review_text"]
+        rating = flask.request.form.get('rating')
+        review = Review(review_text=review_text, film_id=movie_id, rating=rating, user_name=flask.session.get("username"))
+        db.session.add(review)
+        db.session.commit()
 
-        # Redirect to the movie details page for the specified movie ID
         return flask.redirect(flask.url_for("index"))
 
     else:
-        # Render the review page template
         return flask.render_template("review.html", movie_id=movie_id, movie_title=get_movie_by_id(movie_id)["title"])
+    
+@app.route("/signout")
+def signout():
+    flask.session.pop("user_id", None)
+    flask.session.pop("username", None)
+
+    return flask.redirect(flask.url_for("index"))
 
 
 app.run(debug=True)
